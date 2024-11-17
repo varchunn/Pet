e9eebb59d41f190ca03064d698e3cd64a3138a3c
varchunn <varvaram186@gmail.com>
2024-11-18 00:54:50 +0200

    Update main.cpp

--- Pet/main.cpp
+++ Pet/main.cpp
@@ -1,14 +1,121 @@
-//
-//  main.cpp
-//  Pet
-//
-//  Created by Варя Мінєнкова on 18.11.2024.
-//
-
 #include <iostream>
+#include <windows.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Pet
+{
+protected:
+    char* name;
+public:
+    Pet(const char* Name) 
+    {
+        name = new char[strlen(Name) + 1];
+        strcpy_s(name, strlen(Name) + 1, Name);
+    }
+
+    virtual void Sound() const = 0;
+    virtual void Show() const 
+    {
+        cout << "Name: " << name << endl;
+    }
+    virtual void Type() const = 0;
+
+    virtual ~Pet() 
+    {
+        if (name != nullptr) 
+        {
+            delete[] name;
+        }
+    }
+};
+
+class Dog : public Pet 
+{
+public:
+    Dog(const char* Name) : Pet(Name) {}
+
+    void Sound() const
+    {
+        cout << "Woof" << endl;
+    }
+
+    void Type() const
+    {
+        cout << "Dog" << endl;
+    }
+};
+
+class Cat : public Pet {
+public:
+    Cat(const char* Name) : Pet(Name) {}
+
+    void Sound() const
+    {
+        cout << "Meow" << endl;
+    }
+
+    void Type() const
+    {
+        cout << "Cat" << endl;
+    }
+};
+
+class Parrot : public Pet 
+{
+public:
+    Parrot(const char* Name) : Pet(Name) {}
+
+    void Sound() const
+    {
+        cout << "Kaar" << endl;
+    }
+
+    void Type() const
+    {
+        cout << "Parrot" << endl;
+    }
+};
+
+class Hamster : public Pet 
+{
+public:
+    Hamster(const char* Name) : Pet(Name) {}
+
+    void Sound() const
+    {
+        cout << "Squi" << endl;
+    }
+
+    void Type() const
+    {
+        cout << "Hamster" << endl;
+    }
+};
+
+int main() 
+{
+   
+    Pet* pets[4] = 
+    {
+        new Dog("Lucky"),
+        new Cat("Motya"),
+        new Parrot("Patrick"),
+        new Hamster("Buddy")
+    };
+
+    for (int i = 0; i < 4; ++i) 
+    {
+        pets[i]->Show();
+        pets[i]->Type();
+        pets[i]->Sound();
+        cout << endl;
+    }
+
+    for (int i = 0; i < 4; ++i)
+    {
+        delete pets[i];
+    }
+
+    system("pause");
     return 0;
 }
