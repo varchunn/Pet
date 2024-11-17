65a66c4c8d45e6bf7464d317f442ef4ad2ec2d79
varchunn <varvaram186@gmail.com>
2024-11-18 00:50:38 +0200

    Initial Commit

---
+++ Pet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pet
+//
+//  Created by Варя Мінєнкова on 18.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
